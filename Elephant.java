d22fa7ed2ed937501e62126a8c50f0daae81768c
asmitlade <asmitlade.system@gmail.com>
2018-12-01 16:47:22 +0530

    create Elephant.java as subclass of Animal with implementing eat() and sleep() methods

---
+++ Elephant.java
@@ -0,0 +1,8 @@
+class Elephant implements Animal {
+	public void eat(){
+		System.out.println("Elephant eat fruit");
+	}
+	public void sleep(){
+		System.out.println("Elephant sleep anywhere");
+	}
+}
